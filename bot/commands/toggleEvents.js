e3a872bdffea01359fd712a1ee61892c02d597ce
Zaph-x <mikkel@luceartfoto.dk>
2019-11-12 21:24:19 +0100

    Fixed event mention text

--- bot/commands/toggleEvents.js
+++ bot/commands/toggleEvents.js
@@ -4,7 +4,7 @@ const Discord = require('discord.js');
 module.exports.execute = async (client, message, args) => {
     if (message.member.roles.has(config.bot.roles.staff)) {
         let role =  message.guild.roles.get(config.bot.roles.event)
-        role.setMentionable(!role.mentionable, 'Event toggle command').catch(console.error);;
+        await role.setMentionable(!role.mentionable, 'Event toggle command').catch(console.error);;
         await message.channel.send(`Event role is ${(role.mentionable) ? 'now' : 'no longer'} mentionable.`)
     } else {
         await message.channel.send('You do not have permissions for that.')
