4e1070d239bd1db02070edc554b9ef47fe7c786b
Zaph-x <mikkel@luceartfoto.dk>
2020-03-14 22:20:10 +0100

    Added ping command

--- bot/commands/ping.js
+++ bot/commands/ping.js
@@ -1,7 +1,16 @@
 
 
 module.exports.execute = async (client, message, args) => {
-
+    message.channel.send({
+        embed: {
+            color: 0x2ed32e,
+            fields: [{
+                name: "Ping, Pong!",
+                value: "My Ping: " + Math.round(client.ping) + ' ms'
+            }
+            ],
+        }
+    });
 }
 
 module.exports.config = {
