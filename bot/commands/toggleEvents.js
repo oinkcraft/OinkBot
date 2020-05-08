2fcdde72daa3220c9553486d5afe18d55f7e8f1d
Zaph-x <mikkel@luceartfoto.dk>
2020-05-08 10:09:08 +0200

    Fixed event role

--- bot/commands/toggleEvents.js
+++ bot/commands/toggleEvents.js
@@ -2,9 +2,9 @@ const config = require('../../config.json');
 const Discord = require('discord.js');
 
 module.exports.execute = async (client, message, args) => {
-    if (message.member.roles.has(config.bot.roles.staff)) {
+    if (message.member.roles.cache.has(config.bot.roles.staff)) {
         let role =  message.guild.roles.cache.get(config.bot.roles.event)
-        await role.setMentionable(!role.mentionable, 'Event toggle command').catch(console.error);;
+        await role.setMentionable(!role.mentionable, 'Event toggle command').catch(console.error);
         await message.channel.send(`Event role is ${(role.mentionable) ? 'now' : 'no longer'} mentionable.`)
     } else {
         await message.channel.send('You do not have permissions for that.')
