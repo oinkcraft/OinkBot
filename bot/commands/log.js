86bc9740e8df13ace16619ecbdfac8d21121cd66
Zaph-x <mikkel@luceartfoto.dk>
2020-08-15 10:56:58 +0200

    Fixed log fetching from output buffers
    Signed-off-by: Zaph-x <mikkel@luceartfoto.dk>

--- bot/commands/log.js
+++ bot/commands/log.js
@@ -3,13 +3,17 @@ const config = require('../../config.json');
 module.exports.execute = async (client, message, args) => {
     if (message.member.roles.cache.has(config.bot.roles.staff)) {
         if (args.length == 0) {
-            await message.channel.send('STDOUT\n' + '```log\n' + require('child_process').execSync('cat ~/.pm2/logs/app-out.log | tail -n 50').toString() + '\n```');
-            await message.channel.send('STDERR\n' + '```log\n' + require('child_process').execSync('cat ~/.pm2/logs/app-error.log | tail -n 50').toString() + '\n```');
+            await message.channel.send('STDOUT');
+            await message.channel.send(require('child_process').execSync('cat ~/.pm2/logs/app-out.log | tail -n 50').toString(), { code: 'log' });
+            await message.channel.send('STDERR');
+            await message.channel.send(require('child_process').execSync('cat ~/.pm2/logs/app-error.log | tail -n 30').toString(), { code: 'log' });
         }
         else if (args.length == 1) {
             if (!isNaN(args[0])) {
-                await message.channel.send('STDOUT\n' + '```log\n' + require('child_process').execSync('cat ~/.pm2/logs/app-out.log | tail -n ' + args[0]).toString() + '\n```');
-                await message.channel.send('STDERR\n' + '```log\n' + require('child_process').execSync('cat ~/.pm2/logs/app-error.log | tail -n ' + args[0]).toString() + '\n```');
+                await message.channel.send('STDOUT');
+                await message.channel.send(require('child_process').execSync('cat ~/.pm2/logs/app-out.log | tail  -n ' + args[0]).toString(), { code: 'log' });
+                await message.channel.send('STDERR');
+                await message.channel.send(require('child_process').execSync('cat ~/.pm2/logs/app-error.log | tail -n  -n ' + args[0]).toString(), { code: 'log' });
             } else {
                 await message.channel.send(args[0] + 'was not a number. Please provide a number for this command.');
             }
