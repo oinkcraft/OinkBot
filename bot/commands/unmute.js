f0534fa136278646119e2bd089f413fd54af1aa0
Nepgupmixpro <58635917+Nepgupmixpro@users.noreply.github.com>
2020-10-01 19:10:45 -0400

    Mute and unmute command
    Adding a mute and unmute command. Everything should work. I have tested on my bot and it works just fine.

---
+++ bot/commands/unmute.js
@@ -0,0 +1,21 @@
+const config = require('../../config.json');
+const Discord = require('discord.js');
+
+module.exports.execute = async (client, message, args) => {
+    if (message.member.roles.cache.has(config.bot.roles.staff)) {
+        if (args.length == 0) { await message.channel.send('Mention someone to unmute!') } else {
+            let role = message.guild.roles.cache.get(config.bot.roles.muted)
+            let user = message.mentions.members.first()
+            user.roles.remove(role)
+            await message.channel.send(`Unmuted user: ${user}`)
+        }
+    } else {
+        await message.channel.send('You do not have permissions for that.')
+    }
+}
+
+module.exports.config = {
+    name: 'unmute user',
+    aliases: ['unmute', 'unmute user', 'user unmute'],
+    description: 'When someone has finished their mute sentence you can unmute them with this command!',
+}
